cb7a2d59cfe527772255522166bb3dcea7e07d5a
PC <PC@INFO5-2561>
2024-02-09 09:46:51 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AS2324.Guardabascio.Riccardo._3G.Rubrica2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
